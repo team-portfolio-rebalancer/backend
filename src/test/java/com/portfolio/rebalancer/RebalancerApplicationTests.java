90b61666b20de5846c01dd2112417f0beda3e152
shindong96 <2006peter@naver.com>
2024-04-15 22:23:09 +0900

    Spring Project Initialize

---
+++ src/test/java/com/portfolio/rebalancer/RebalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.rebalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
