64de4c4f0d960455f1c69f75a1bbdd4cead7848a
shindong96 <2006peter@naver.com>
2024-04-25 17:18:25 +0900

    refactor: 메세지 추출, userId 검증 NotNull로 변경

--- src/main/java/com/portfolio/rebalancer/dto/request/CategoryRequest.java
+++ src/main/java/com/portfolio/rebalancer/dto/request/CategoryRequest.java
@@ -2,6 +2,7 @@ package com.portfolio.rebalancer.dto.request;
 
 import com.portfolio.rebalancer.domain.Color;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,12 +13,13 @@ import lombok.NoArgsConstructor;
 @Getter
 public class CategoryRequest {
 
+    @NotNull(message = InvalidInputMessage.BLANK)
     private Long userId;
 
-    @NotBlank
+    @NotBlank(message = InvalidInputMessage.BLANK)
     private String name;
 
-    @NotBlank
-    @Pattern(regexp = Color.REGEX, message = "색상 형식이 맞지 않습니다.")
+    @NotBlank(message = InvalidInputMessage.BLANK)
+    @Pattern(regexp = Color.REGEX, message = InvalidInputMessage.COLOR_REGEX)
     private String color;
 }
---
+++ src/main/java/com/portfolio/rebalancer/dto/request/InvalidInputMessage.java
@@ -0,0 +1,6 @@
+package com.portfolio.rebalancer.dto.request;
+
+public class InvalidInputMessage {
+    static final String BLANK = "입력값이 없습니다.";
+    static final String COLOR_REGEX = "색상 형식이 맞지 않습니다.";
+}
