a36655d181faf3e958181484b6495e258e70a7ae
Juwon Park <devprofessionalism@gmail.com>
2024-06-23 22:21:42 +0900

    feat: Category 조회 API 개발

--- src/main/java/com/portfolio/rebalancer/application/CategoryService.java
+++ src/main/java/com/portfolio/rebalancer/application/CategoryService.java
@@ -1,11 +1,17 @@
 package com.portfolio.rebalancer.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.portfolio.rebalancer.domain.category.Category;
+import com.portfolio.rebalancer.domain.category.CategoryErrorCode;
 import com.portfolio.rebalancer.domain.category.CategoryRepository;
+import com.portfolio.rebalancer.domain.exception.RebalancerException;
 import com.portfolio.rebalancer.dto.request.CategoryRequest;
+import com.portfolio.rebalancer.dto.response.CategoryResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +27,18 @@ public class CategoryService {
 		Category category = new Category(userId, request.getName(), request.getColor());
 		Category savedCategory = categoryRepository.save(category);
 		return savedCategory.getId();
+	}
+
+	public CategoryResponse findById(final Long id) {
+		Category category = categoryRepository.findById(id)
+			.orElseThrow(() -> new RebalancerException(CategoryErrorCode.CATEGORY_NOT_FOUND));
+		return CategoryResponse.from(category);
+	}
+
+	public List<CategoryResponse> findAll() {
+		List<Category> categories = categoryRepository.findAll();
+		return categories.stream()
+			.map(CategoryResponse::from)
+			.collect(Collectors.toList());
 	}
 }
--- src/main/java/com/portfolio/rebalancer/presentation/CategoryControllerDocs.java
+++ src/main/java/com/portfolio/rebalancer/presentation/CategoryControllerDocs.java
@@ -1,9 +1,12 @@
 package com.portfolio.rebalancer.presentation;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.portfolio.rebalancer.dto.request.CategoryRequest;
+import com.portfolio.rebalancer.dto.response.CategoryResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -22,45 +25,80 @@ public interface CategoryControllerDocs {
 			examples = {
 				@ExampleObject(name = "유저 아이디가 입력되지 않음", value = """
 					{
-					    "success": false,
-					    "error": {
-					        "code": "INPUT_001",
-					        "message": "회사명이 입력되지 않았습니다."
-					    },
-					    "data": null
+						"success": false,
+						"error": {
+							"code": "INPUT_001",
+							"message": "회사명이 입력되지 않았습니다."
+						},
+						"data": null
 					}
 					"""),
 				@ExampleObject(name = "이름이 입력되지 않음", value = """
 					{
-					    "success": false,
-					    "error": {
-					        "code": "INPUT_001",
-					        "message": "입력값이 없습니다."
-					    },
-					    "data": null
+						"success": false,
+						"error": {
+							"code": "INPUT_001",
+							"message": "입력값이 없습니다."
+						},
+						"data": null
 					}
 					"""),
 				@ExampleObject(name = "색상이 입력되지 않음", value = """
 					{
-					    "success": false,
-					    "error": {
-					        "code": "INPUT_001",
-					        "message": "입력값이 없습니다."
-					    },
-					    "data": null
+						"success": false,
+						"error": {
+							"code": "INPUT_001",
+							"message": "입력값이 없습니다."
+						},
+						"data": null
 					}
 					"""),
 				@ExampleObject(name = "색상 형식이 잘못됨", value = """
 					{
-					    "success": false,
-					    "error": {
-					        "code": "INPUT_002",
-					        "message": "색상 형식이 맞지 않습니다."
-					    },
-					    "data": null
+						"success": false,
+						"error": {
+							"code": "INPUT_002",
+							"message": "색상 형식이 맞지 않습니다."
+						},
+						"data": null
 					}
 					""")
 			}))
 	})
 	ResponseEntity<Void> create(@RequestBody final CategoryRequest request);
+
+	@Operation(summary = "카테고리 상세 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "카테고리 상세 조회 성공"),
+		@ApiResponse(responseCode = "400", description = "입력 형식이 잘못됨", content = @Content(
+			examples = {
+				@ExampleObject(name = "아이디가 입력되지 않음", value = """
+					{
+						"success": false,
+						"error": {
+							"code": "INPUT_001",
+							"message": "입력값이 없습니다."
+						},
+						"data": null
+					}
+					"""),
+				@ExampleObject(name = "아이디 값이 잘못됨", value = """
+					{
+						"success": false,
+						"error": {
+							"code": "CATEGORY_001",
+							"message": "해당 id의 카테고리가 없습니다."
+						},
+						"data": null
+					}
+					""")
+			}))
+	})
+	ResponseEntity<CategoryResponse> findById(Long id);
+
+	@Operation(summary = "카테고리 전체 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "카테고리 전체 조회 성공")
+	})
+	ResponseEntity<List<CategoryResponse>> findAll();
 }
