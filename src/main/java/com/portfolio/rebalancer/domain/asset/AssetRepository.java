bb3acfbd42c9780789017fbbdf7100a47e1ffb93
Juwon Park <devprofessionalism@gmail.com>
2024-07-20 16:39:25 +0900

    refactor: findOrSaveAsset 함수 따로 안빼고, save안에 한 흐름으로 읽기 쉽게 리팩토링

--- src/main/java/com/portfolio/rebalancer/domain/asset/AssetRepository.java
+++ src/main/java/com/portfolio/rebalancer/domain/asset/AssetRepository.java
@@ -1,8 +1,10 @@
 package com.portfolio.rebalancer.domain.asset;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssetRepository extends JpaRepository<Asset, Long> {
 
-	Asset findByCode(String code);
+	Optional<Asset> findByCode(String code);
 }
